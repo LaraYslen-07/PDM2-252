21e8b996ed8e35d2f0a076fe7a973977ce8d4d03
LaraYslen-07 <yslenlaragb@gmail.com>
2025-10-16 13:43:43 -0300

    Remove arquivos e pastas antigas, mantendo apenas atividades e exercicios

--- test/iot_simulacao_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:meu_app_web/meu_app_web.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('main executa sem lançar exceções', () async {
-    expect(() async => main(), returnsNormally);
-  });
-}
